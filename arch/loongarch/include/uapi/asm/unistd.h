7915ad79cd3e231d2b318e47412f15e19534cba8
Fanrui Meng <mengfanrui@loongson.cn>
2022-05-25 11:17:05 +0800

    LoongArch32r : modify la32r compile toolchain config.
    	1. la32r toolchain config modification
    	2. la32r libc need futex(TIME32_SYSCALL)
    
    Signed-off-by: Fanrui Meng <mengfanrui@loongson.cn>

--- arch/loongarch/include/uapi/asm/unistd.h
+++ arch/loongarch/include/uapi/asm/unistd.h
@@ -4,7 +4,7 @@
 #define __ARCH_WANT_SYS_CLONE3
 #define __ARCH_WANT_SET_GET_RLIMIT
 
-#ifdef __loongarch32
+#ifdef __loongarch32r
 #define __ARCH_WANT_STAT64
 #define __ARCH_WANT_TIME32_SYSCALLS
 #endif

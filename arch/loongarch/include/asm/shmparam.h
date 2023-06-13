e69cb1556958c6b9141c1d88dc825c98d91c080a
Jinyang Shen <shenjinyang@loongson.cn>
2023-06-13 15:30:23 +0800

    LoongArch32r: fix SHMLBA in la32
    Modify the SHMLBA in la32 from 64K to 16K to align it with the 4K page
    size of la32, this fix the ltp test: shmt09.
    
    Signed-off-by: Jinyang Shen <shenjinyang@loongson.cn>

--- arch/loongarch/include/asm/shmparam.h
+++ arch/loongarch/include/asm/shmparam.h
@@ -7,6 +7,10 @@
 
 #define __ARCH_FORCE_SHMLBA	1
 
+#ifdef CONFIG_32BIT
+#define	SHMLBA	SZ_16K		 /* attach addr a multiple of this */
+#else
 #define	SHMLBA	SZ_64K		 /* attach addr a multiple of this */
+#endif
 
 #endif /* _ASM_SHMPARAM_H */
